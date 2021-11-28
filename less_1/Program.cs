22d7db86281a9eeb0d6f08ab86842ea1bb9b9ecf
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 15:56:42 +0500

    Добавьте файлы проекта.

---
+++ less_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace less_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age = 27;
+            string lastName = "Jordan";
+            char firstCharacter = lastName[0];
+            double waterTemperature = 12.102;
+            uint idUser = 1111111;
+            float speedMainHero = 12.2f;
+            ulong numberOfSouls = 1277721; //game "souls like" сurrency
+            bool isShoot = true;
+            int xPosition = 10000;
+            short yPosition = 1123;
+        }
+    }
+}
